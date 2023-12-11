675261d030c6eff451e923380b85270541bcc61c
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2023-12-10 21:45:44 -0600

    prueba

--- ubereats.py
+++ ubereats.py
@@ -28,132 +28,134 @@ driver.set_window_size(1920, 1080)
 wait = WebDriverWait(driver, 10)
 
 
-def basica(address, producto):
 
 
-    # Navigate to the initial URL
-    #url="https://www.ubereats.com"
-    url="https://www.ubereats.com/category-feed/Shop?mod=locationManager&modctx=feed&next=%2Fcategory-feed%2FShop%3Fpl%3DJTdCJTIyYWRkcmVzcyUyMiUzQSUyMkVqZSUyMHZpYWwlMjA0JTIwU3VyJTIwWG9sYSUyMDE5NSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMmY0OGYwNmQ2LTcyMjEtNzk0ZS1lODE4LTI5NTIxY2JlN2NlMCUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJ1YmVyX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBMTkuMzkzOSUyQyUyMmxvbmdpdHVkZSUyMiUzQS05OS4xMzg3MTQlN0Q%253D%26ps%3D1%26sc%3DSHORTCUTS&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkVqZSUyMHZpYWwlMjA0JTIwU3VyJTIwWG9sYSUyMDE5NSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMmY0OGYwNmQ2LTcyMjEtNzk0ZS1lODE4LTI5NTIxY2JlN2NlMCUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJ1YmVyX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBMTkuMzkzOSUyQyUyMmxvbmdpdHVkZSUyMiUzQS05OS4xMzg3MTQlN0Q%3D&ps=1&sc=SHORTCUTS"
-    driver.get(url)
-    print("Buscando", producto, "en los supermercados de Uber Eats para la ubicación ", address,". Por favor espere⏳...")
+# def basica(address, producto):
 
-    # Set the location
-    try:
-        control_direct = wait.until(
-            EC.element_to_be_clickable((By.ID, "location-typeahead-location-manager-input"))
-        )
-        control_direct.clear()
-        cp=address
-        control_direct.send_keys(address)
-        time.sleep(3)
-        control_direct.send_keys(Keys.RETURN)
-    except Exception as e:
-        print("Error:", e)
-        driver.quit()
-        return
-    time.sleep(3)
 
-    # Navigate to the grocery page
-    grocery = "https://www.ubereats.com/category-feed/Grocery?stores=all"
-    driver.get(grocery)
-    time.sleep(3)
+#     # Navigate to the initial URL
+#     #url="https://www.ubereats.com"
+#     url="https://www.ubereats.com/category-feed/Shop?mod=locationManager&modctx=feed&next=%2Fcategory-feed%2FShop%3Fpl%3DJTdCJTIyYWRkcmVzcyUyMiUzQSUyMkVqZSUyMHZpYWwlMjA0JTIwU3VyJTIwWG9sYSUyMDE5NSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMmY0OGYwNmQ2LTcyMjEtNzk0ZS1lODE4LTI5NTIxY2JlN2NlMCUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJ1YmVyX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBMTkuMzkzOSUyQyUyMmxvbmdpdHVkZSUyMiUzQS05OS4xMzg3MTQlN0Q%253D%26ps%3D1%26sc%3DSHORTCUTS&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkVqZSUyMHZpYWwlMjA0JTIwU3VyJTIwWG9sYSUyMDE5NSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMmY0OGYwNmQ2LTcyMjEtNzk0ZS1lODE4LTI5NTIxY2JlN2NlMCUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJ1YmVyX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBMTkuMzkzOSUyQyUyMmxvbmdpdHVkZSUyMiUzQS05OS4xMzg3MTQlN0Q%3D&ps=1&sc=SHORTCUTS"
+#     driver.get(url)
+#     print("Buscando", producto, "en los supermercados de Uber Eats para la ubicación ", address,". Por favor espere⏳...")
 
-    # Extract store links
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    store_links = soup.find_all('a', {'data-testid': 'store-card'})
-    stores = [{'name': link.find('h3').get_text(), 'url': link['href']} for link in store_links]
+#     # Set the location
+#     try:
+#         control_direct = wait.until(
+#             EC.element_to_be_clickable((By.ID, "location-typeahead-location-manager-input"))
+#         )
+#         control_direct.clear()
+#         cp=address
+#         control_direct.send_keys(address)
+#         time.sleep(3)
+#         control_direct.send_keys(Keys.RETURN)
+#     except Exception as e:
+#         print("Error:", e)
+#         driver.quit()
+#         return
+#     time.sleep(3)
 
-    # DataFrame for stores
-    df_stores = pd.DataFrame(stores)
-    df_stores=pd.DataFrame(stores)
-    #Dejar lo que está después de /store/ y antes del segundo /
-    #Hacer copia de de url
-    df_stores['sucursal']=df_stores['url'].copy()
-    df_stores['sucursal']=df_stores['sucursal'].str.split('/store/').str[1]
-    #Quitar lo que está después del segundo /
-    df_stores['sucursal']=df_stores['sucursal'].str.split('/').str[0]
-    #Eliminar "-" y reemplazar por espacio
-    df_stores['sucursal']=df_stores['sucursal'].str.replace('-',' ')
-    #Completar la url
-    df_stores['url']='https://www.ubereats.com'+df_stores['url']
-    #Dejar solo si contienen la palabra "Soriana", "Sumesa","City Market", "Comer", "Chedraui"
-    #df_stores=df_stores[df_stores['name'].str.contains('Soriana|Sumesa|City Market|Comer|Chedraui')]
-    #Añadir columna de búsqueda de código postal
-    df_stores['cp']=cp
+#     # Navigate to the grocery page
+#     grocery = "https://www.ubereats.com/category-feed/Grocery?stores=all"
+#     driver.get(grocery)
+#     time.sleep(3)
 
-    # Initialize lists for data collection
-    prod = []
-    precios = []
-    tienda = []
-    sucursal = []
+#     # Extract store links
+#     html = driver.page_source
+#     soup = BeautifulSoup(html, 'html.parser')
+#     store_links = soup.find_all('a', {'data-testid': 'store-card'})
+#     stores = [{'name': link.find('h3').get_text(), 'url': link['href']} for link in store_links]
 
-    # Search for each product in every store
-    for index, row in tqdm(df_stores.iterrows(), total=df_stores.shape[0], desc="Recolectando productos y precios😊"):
+#     # DataFrame for stores
+#     df_stores = pd.DataFrame(stores)
+#     df_stores=pd.DataFrame(stores)
+#     #Dejar lo que está después de /store/ y antes del segundo /
+#     #Hacer copia de de url
+#     df_stores['sucursal']=df_stores['url'].copy()
+#     df_stores['sucursal']=df_stores['sucursal'].str.split('/store/').str[1]
+#     #Quitar lo que está después del segundo /
+#     df_stores['sucursal']=df_stores['sucursal'].str.split('/').str[0]
+#     #Eliminar "-" y reemplazar por espacio
+#     df_stores['sucursal']=df_stores['sucursal'].str.replace('-',' ')
+#     #Completar la url
+#     df_stores['url']='https://www.ubereats.com'+df_stores['url']
+#     #Dejar solo si contienen la palabra "Soriana", "Sumesa","City Market", "Comer", "Chedraui"
+#     #df_stores=df_stores[df_stores['name'].str.contains('Soriana|Sumesa|City Market|Comer|Chedraui')]
+#     #Añadir columna de búsqueda de código postal
+#     df_stores['cp']=cp
 
-        store_url = row['url']
-        store_name = row['name']
-        store_sucursal = row['sucursal']
-        driver.get(store_url)
+#     # Initialize lists for data collection
+#     prod = []
+#     precios = []
+#     tienda = []
+#     sucursal = []
 
-        # Search for the product
-        time.sleep(3)
-        try:
-            product_search = driver.find_element(By.ID, "search-suggestions-typeahead-input")
-            product_search.clear()
-            product_search.send_keys(producto)
-            time.sleep(3)
-            product_search.send_keys(Keys.ENTER)
-            time.sleep(3)
+#     # Search for each product in every store
+#     for index, row in tqdm(df_stores.iterrows(), total=df_stores.shape[0], desc="Recolectando productos y precios😊"):
 
-            # Extract product info
-            html = driver.page_source
-            soup = BeautifulSoup(html, 'html.parser')
-            product_items = soup.find_all('div', attrs={'data-testid': lambda value: value and value.startswith('store-menu-item')})
+#         store_url = row['url']
+#         store_name = row['name']
+#         store_sucursal = row['sucursal']
+#         driver.get(store_url)
+
+#         # Search for the product
+#         time.sleep(3)
+#         try:
+#             product_search = driver.find_element(By.ID, "search-suggestions-typeahead-input")
+#             product_search.clear()
+#             product_search.send_keys(producto)
+#             time.sleep(3)
+#             product_search.send_keys(Keys.ENTER)
+#             time.sleep(3)
+
+#             # Extract product info
+#             html = driver.page_source
+#             soup = BeautifulSoup(html, 'html.parser')
+#             product_items = soup.find_all('div', attrs={'data-testid': lambda value: value and value.startswith('store-menu-item')})
         
-            for item in product_items:
-                # Extract rich-text elements, which should contain the price and product name
-                rich_texts = item.find_all('span', {'data-testid': 'rich-text'})
-                if len(rich_texts) >= 2:  # Make sure there are at least two rich-text elements
-                    try:
-                        price = rich_texts[0].get_text(strip=True)
-                        prod_name = rich_texts[1].get_text(strip=True)
-                        prod.append(prod_name)
-                        precios.append(price)
-                        tienda.append(store_name)
-                        sucursal.append(store_sucursal)
-                    except Exception as e:
-                        print(f"Error extracting data for store: {store_name} - {e}")
-                        prod.append(None)
-                        precios.append(None)
-                        tienda.append(store_name)
-                        sucursal.append(store_sucursal)
-                else:
-                    # If not enough span elements found, append None values
-                    prod.append(None)
-                    precios.append(None)
-                    tienda.append(store_name)
-                    sucursal.append(store_sucursal)
+#             for item in product_items:
+#                 # Extract rich-text elements, which should contain the price and product name
+#                 rich_texts = item.find_all('span', {'data-testid': 'rich-text'})
+#                 if len(rich_texts) >= 2:  # Make sure there are at least two rich-text elements
+#                     try:
+#                         price = rich_texts[0].get_text(strip=True)
+#                         prod_name = rich_texts[1].get_text(strip=True)
+#                         prod.append(prod_name)
+#                         precios.append(price)
+#                         tienda.append(store_name)
+#                         sucursal.append(store_sucursal)
+#                     except Exception as e:
+#                         print(f"Error extracting data for store: {store_name} - {e}")
+#                         prod.append(None)
+#                         precios.append(None)
+#                         tienda.append(store_name)
+#                         sucursal.append(store_sucursal)
+#                 else:
+#                     # If not enough span elements found, append None values
+#                     prod.append(None)
+#                     precios.append(None)
+#                     tienda.append(store_name)
+#                     sucursal.append(store_sucursal)
 
 
 
-        except Exception as e:
-            print(f"Producto no encontrado en tienda {store_name}")
+#         except Exception as e:
+#             print(f"Producto no encontrado en tienda {store_name}")
 
-    # Create DataFrame from collected data
-    df = pd.DataFrame({'producto': prod, 'precio': precios, 'tienda': tienda, 'sucursal': sucursal, 'direccion_busca': cp,'fecha_consulta':pd.to_datetime('today')})
-    df["precio"]=df["precio"].str.replace("MX$","")
-    #Eliminar comas
-    df["precio"]=df["precio"].str.replace(",","")
-    #Split columna de precio en dos columnas si tiene "/"
-    df[["precio","unidad"]]=df["precio"].str.split("/",expand=True)
-    #Transformar columna de precio a float
-    df["precio"]=pd.to_numeric(df["precio"])
-    df['producto']=df['producto'].str.lower()
-    #Quitar acentos 
-    df['producto']=df['producto'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
-    driver.quit()
-    return df
+#     # Create DataFrame from collected data
+#     df = pd.DataFrame({'producto': prod, 'precio': precios, 'tienda': tienda, 'sucursal': sucursal, 'direccion_busca': cp,'fecha_consulta':pd.to_datetime('today')})
+#     df["precio"]=df["precio"].str.replace("MX$","")
+#     #Eliminar comas
+#     df["precio"]=df["precio"].str.replace(",","")
+#     #Split columna de precio en dos columnas si tiene "/"
+#     df[["precio","unidad"]]=df["precio"].str.split("/",expand=True)
+#     #Transformar columna de precio a float
+#     df["precio"]=pd.to_numeric(df["precio"])
+#     df['producto']=df['producto'].str.lower()
+#     #Quitar acentos 
+#     df['producto']=df['producto'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+#     driver.quit()
+#     return df
 
 
 
@@ -161,10 +163,11 @@ def avanzada(url, producto):
     # Initialize lists to store data
     prod = []
     precios = []
-    tienda = []
-    sucursal = []
+    # tienda = []
+    # sucursal = []
 
 
+    #Abrir la url ingresada
     driver.get(url)
     print("Buscando ", producto, " en el supermercado, por favor espere⏳...")
     # # Allow time for the page to load

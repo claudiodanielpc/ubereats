0357eb48517ac1fa8ba157bb14ae2ecdccba4609
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2023-12-11 22:25:33 -0600

    Se actualiza setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
        install_requires=[
         'pandas',
-        'geopandas'
+        'geopandas',
         'bs4',
         'requests',
         'selenium',
